0a1b2a2a87701eb3579bf394ec116084d32daf70
swmtmply <delosreyesjohnallen@gmail.com>
2022-09-27 11:40:14 +0800

    ⚡ feat: added register api

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,22 +3,55 @@ import NextAuth, { type NextAuthOptions } from "next-auth";
 // Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { prisma } from "../../../server/db/client";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
+import { env } from "../../../env/server.mjs";
 
 export const authOptions: NextAuthOptions = {
   // Include user.id on session
   callbacks: {
-    session({ session, user }) {
+    session({ session, token }) {
       if (session.user) {
-        session.user.id = user.id;
+        session.user.id = token.sub as string;
       }
       return session;
     },
   },
+  pages: {
+    signIn: "/auth/signin",
+  },
+  session: { strategy: "jwt" },
+  secret: env.NEXTAUTH_SECRET,
   // Configure one or more authentication providers
   adapter: PrismaAdapter(prisma),
   providers: [
-    // TODO add credentials provider
-    // ...add more providers here
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: { type: "text" },
+        password: { type: "password" },
+      },
+      async authorize(credentials) {
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials?.email,
+          },
+        });
+
+        if (user !== null) {
+          const isValid = bcrypt.compareSync(
+            credentials?.password ?? "",
+            user.password
+          );
+
+          if (isValid) {
+            return user;
+          }
+          throw new Error("Incorrect password. Please try again.");
+        }
+        throw new Error("Invalid credentials. Please try again.");
+      },
+    }),
   ],
 };
 
--- src/server/trpc/router/auth.ts
+++ src/server/trpc/router/auth.ts
@@ -1,10 +1,63 @@
-import { t, authedProcedure } from "../trpc";
+import { TRPCError } from "@trpc/server";
+import { z } from "zod";
+import { t } from "../trpc";
 
 export const authRouter = t.router({
-  getSession: t.procedure.query(({ ctx }) => {
-    return ctx.session;
-  }),
-  getSecretMessage: authedProcedure.query(() => {
-    return "You are logged in and can see this secret message!";
-  }),
+  getSession: t.procedure.query(({ ctx }) => ctx.session),
+  register: t.procedure
+    .input(
+      z.object({
+        name: z.string(),
+        email: z.string(),
+        password: z.string(),
+        user_type: z.string().nullish(),
+        image: z.string().nullish(),
+        profile: z.object({
+          first_name: z.string(),
+          last_name: z.string(),
+          middle_name: z.string().nullish(),
+          suffix: z.string().nullish(),
+          date_of_birth: z.date().nullish(),
+          phone_no: z.string().nullish(),
+          gender: z.string().nullish(),
+        }),
+        address: z
+          .object({
+            street: z.string().nullish(),
+            city: z.string().nullish(),
+            state: z.string().nullish(),
+            zip: z.string().nullish(),
+            country: z.string().nullish(),
+          })
+          .nullish(),
+      })
+    )
+    .mutation(async ({ input, ctx }) => {
+      try {
+        await ctx.prisma.user.create({
+          data: {
+            name: input.name,
+            email: input.email,
+            password: input.password,
+            image: input.image,
+            user_type: input.user_type,
+            username: (
+              input.profile.first_name[0] + input.profile.last_name
+            ).toLowerCase(),
+            profile: {
+              create: input.profile,
+            },
+            address: {
+              create: input.address ?? undefined,
+            },
+          },
+        });
+        return "User created successfully";
+      } catch (error) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: JSON.stringify(error),
+        });
+      }
+    }),
 });

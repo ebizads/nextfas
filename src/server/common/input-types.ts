cc064c7e5689fff8df10ad319412d36124e21eb1
swmtmply <delosreyesjohnallen@gmail.com>
2022-10-05 15:57:45 +0800

    🧹chore: refactored asset type input

--- src/server/common/input-types.ts
+++ src/server/common/input-types.ts
@@ -78,18 +78,19 @@ export const AssetCreateInput = z.object({
   name: z.string(),
   number: z.string(),
   description: z.string().nullish(),
-  serial_no: z.string().nullish(),
-  project: z.string().nullish(),
+  serial_number: z.string().nullish(),
   original_cost: z.number().nullish(),
   current_cost: z.number().nullish(),
-  residual_value: z.number().nullish(),
-  residual_value_percentage: z.number().nullish(),
-  status: z.string().nullish(),
-  lifetime: z.number().nullish(),
-  netbook_value: z.number().nullish(),
+  current_netbook_value: z.number().nullish(),
+
   typeId: z.number().nullish(),
   categoryId: z.number().nullish(),
-  userId: z.number().nullish(),
+  custodianId: z.number().nullish(),
+  manufacturerId: z.number().nullish(),
+  supplierId: z.number().nullish(),
+  classId: z.number().nullish(),
+  vendorId: z.number().nullish(),
+
   model: z
     .object({
       name: z.string(),
@@ -104,8 +105,6 @@ export const AssetCreateInput = z.object({
       class: z.string().nullish(),
     })
     .nullish(),
-  manufacturerId: z.number().nullish(),
-  supplierId: z.number().nullish(),
 });
 
 export const AssetEditInput = z.object({
@@ -113,18 +112,19 @@ export const AssetEditInput = z.object({
   name: z.string().optional(),
   number: z.string().optional(),
   description: z.string().optional(),
-  serial_no: z.string().optional(),
-  project: z.string().optional(),
+  serial_number: z.string().optional(),
   original_cost: z.number().optional(),
   current_cost: z.number().optional(),
-  residual_value: z.number().optional(),
-  residual_value_percentage: z.number().optional(),
-  status: z.string().optional(),
-  lifetime: z.number().optional(),
-  netbook_value: z.number().optional(),
+  current_netbook_value: z.number().optional(),
+
   typeId: z.number().optional(),
   categoryId: z.number().optional(),
-  userId: z.number().optional(),
+  custodianId: z.number().optional(),
+  manufacturerId: z.number().optional(),
+  supplierId: z.number().optional(),
+  classId: z.number().optional(),
+  vendorId: z.number().optional(),
+
   model: z
     .object({
       name: z.string(),
@@ -139,8 +139,6 @@ export const AssetEditInput = z.object({
       class: z.string().optional(),
     })
     .optional(),
-  manufacturerId: z.number().optional(),
-  supplierId: z.number().optional(),
 });
 
 export const CategoryCreateInput = z.object({
@@ -157,6 +155,15 @@ export const TypeCreateInput = z.object({
 });
 
 export const TypeEditInput = z.object({
+  id: z.number(),
+  name: z.string().optional(),
+});
+
+export const ClassCreateInput = z.object({
+  name: z.string(),
+});
+
+export const ClassEditInput = z.object({
   id: z.number(),
   name: z.string().optional(),
 });
@@ -242,6 +249,69 @@ export const SupplierEditInput = z.object({
   phone_number: z.string().optional(),
   alt_phone_number: z.string().optional(),
   fax_number: z.string().optional(),
+  address: z
+    .object({
+      street: z.string().optional(),
+      city: z.string().optional(),
+      state: z.string().optional(),
+      zip: z.string().optional(),
+      country: z.string().optional(),
+      shipping_address: z.string().optional(),
+      billing_address: z.string().optional(),
+    })
+    .optional(),
+});
+
+export const EmployeeCreateInput = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  image: z.string().nullish(),
+  profile: z.object({
+    first_name: z
+      .string({ required_error: "First Name is required" })
+      .min(1, { message: "First name is required" }),
+    last_name: z
+      .string({ required_error: "Last Name is required" })
+      .min(1, "Last name is required"),
+    middle_name: z.string().nullish(),
+    suffix: z.string().nullish(),
+    date_of_birth: z.date().nullish(),
+    phone_no: z.string().nullish(),
+    gender: z.string().nullish(),
+  }),
+  address: z
+    .object({
+      street: z.string().nullish(),
+      city: z.string().nullish(),
+      state: z.string().nullish(),
+      zip: z.string().nullish(),
+      country: z.string().nullish(),
+      shipping_address: z.string().nullish(),
+      billing_address: z.string().nullish(),
+    })
+    .nullish(),
+});
+
+export const EmployeeEditInput = z.object({
+  id: z.number(),
+  name: z.string().optional(),
+  email: z.string().email().optional(),
+  image: z.string().optional(),
+  profile: z.object({
+    first_name: z
+      .string()
+      .min(1, { message: "First name is required" })
+      .optional(),
+    last_name: z
+      .string({ required_error: "Last Name is required" })
+      .min(1, "Last name is required")
+      .optional(),
+    middle_name: z.string().optional(),
+    suffix: z.string().optional(),
+    date_of_birth: z.date().optional(),
+    phone_no: z.string().optional(),
+    gender: z.string().optional(),
+  }),
   address: z
     .object({
       street: z.string().optional(),

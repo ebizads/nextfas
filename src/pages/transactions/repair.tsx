7eddc8873d2588b9917fd9ef47e069b8b01225cb
Tenpai10 <pransarbey@gmail.com>
2022-10-05 16:23:19 +0800

    🧹 chore: initial creation of pages

---
+++ src/pages/transactions/repair.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import DashboardLayout from "../../layouts/DashboardLayout"
+
+const AssetRepair = () => {
+  return (
+    <DashboardLayout>
+      <div>Repair of Asset</div>
+    </DashboardLayout>
+  )
+}
+
+export default AssetRepair
---
+++ src/pages/transactions/transfer.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import DashboardLayout from "../../layouts/DashboardLayout"
+
+const AssetTransfer = () => {
+  return (
+    <DashboardLayout>
+      <div>Transfer Asset</div>
+    </DashboardLayout>
+  )
+}
+
+export default AssetTransfer

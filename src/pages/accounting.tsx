4cfb0052b1855ac962e4b7077da43fea2deaa282
Tenpai10 <pransarbey@gmail.com>
2022-10-06 11:15:31 +0800

    🧹 chore: initial route page

---
+++ src/pages/accounting.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import DashboardLayout from "../layouts/DashboardLayout"
+
+const Accounting = () => {
+  return <DashboardLayout>Accounting</DashboardLayout>
+}
+
+export default Accounting
---
+++ src/pages/employees.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import DashboardLayout from "../layouts/DashboardLayout"
+
+const Employees = () => {
+  return <DashboardLayout>Employees</DashboardLayout>
+}
+
+export default Employees

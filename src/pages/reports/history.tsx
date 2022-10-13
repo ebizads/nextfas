95204eb1e675672bb55b7dc8222eae9ab8a2d070
Tenpai10 <pransarbey@gmail.com>
2022-10-13 09:29:31 +0800

    🧹 chore: created report pages

---
+++ src/pages/reports/history.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import DashboardLayout from "../../layouts/DashboardLayout"
+
+const History = () => {
+  return (
+    <DashboardLayout>
+      <></>
+    </DashboardLayout>
+  )
+}
+
+export default History

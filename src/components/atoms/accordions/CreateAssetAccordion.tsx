ca99f2ecc8cfddde1d23a2b44463290a621aaa91
Tenpai10 <pransarbey@gmail.com>
2022-11-09 13:55:13 +0800

    🧹 chore: label and input conventions

--- src/components/atoms/accordions/CreateAssetAccordion.tsx
+++ src/components/atoms/accordions/CreateAssetAccordion.tsx
@@ -9,6 +9,7 @@ import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
 import { AssetCreateInput } from '../../../server/common/schemas/asset';
+import { IconCalendar } from '@tabler/icons';
 
 
 function AdditionalInfo(props: { register: any, errors: any }) {
@@ -45,16 +46,16 @@ function AdditionalInfo(props: { register: any, errors: any }) {
           <div className="col-span-9 grid grid-cols-11 gap-2">
             <div className="col-span-3 space-y-2">
               <p className='text-sm text-gray-700'>Purchase Date</p>
-              <DatePicker dropdownType="modal" size="sm" // value={props.value}
+              <DatePicker allowFreeInput size="sm" // value={props.value}
                 onChange={value => {// setValue("hired_date", value)
                 }} classNames={{
-                  input: 'border-2 border-gray-400 h-11 rounded-md px-2'
+                  input: 'border-2 border-gray-400 h-11 rounded-md'
                 }} // className="peer peer block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent px-3 text-sm text-gray-900 focus:border-tangerine-500 focus:outline-none focus:ring-0"
               />
             </div>
             <div className="col-span-3 space-y-2">
               <p className='text-sm text-gray-700'>Depreciation Start Date</p>
-              <DatePicker dropdownType="modal" size="sm" // value={props.value}
+              <DatePicker allowFreeInput size="sm" // value={props.value}
                 onChange={value => {// setValue("hired_date", value)
                 }} classNames={{
                   input: 'border-2 border-gray-400 h-11 rounded-md px-2'
@@ -63,7 +64,7 @@ function AdditionalInfo(props: { register: any, errors: any }) {
             </div>
             <div className="col-span-3 space-y-2">
               <p className='text-sm text-gray-700'>Depreciation End Date</p>
-              <DatePicker dropdownType="modal" size="sm" // value={props.value}
+              <DatePicker allowFreeInput size="sm" // value={props.value}
                 onChange={value => {// setValue("hired_date", value)
                 }} classNames={{
                   input: 'border-2 border-gray-400 h-11 rounded-md px-2'
@@ -81,7 +82,7 @@ function AdditionalInfo(props: { register: any, errors: any }) {
           </div>
           <div className="col-span-3 space-y-2">
             <p className='text-sm text-gray-700'>Target Depreciation Date</p>
-            <DatePicker dropdownType="modal" size="sm" // value={props.value}
+            <DatePicker allowFreeInput size="sm" // value={props.value}
               onChange={value => {// setValue("hired_date", value)
               }} classNames={{
                 input: 'border-2 border-gray-400 h-11 rounded-md px-2'
@@ -90,7 +91,7 @@ function AdditionalInfo(props: { register: any, errors: any }) {
           </div>
           <div className="col-span-3 space-y-2">
             <p className='text-sm text-gray-700'>Last Depreciation Period</p>
-            <DatePicker dropdownType="modal" size="sm" // value={props.value}
+            <DatePicker allowFreeInput size="sm" // value={props.value}
               onChange={value => {// setValue("hired_date", value)
               }} classNames={{
                 input: 'border-2 border-gray-400 h-11 rounded-md px-2'
@@ -207,7 +208,7 @@ function AssetInfo(props: { register: any, errors: any }) {
       <Accordion.Panel>
         <div className="grid grid-cols-9 gap-2">
           <div className="col-span-3">
-            <InputField register={props.register} label="Serial Number" name="category" />
+            <InputField register={props.register} label="Asset Number" name="category" />
             <AlertInput>{props.errors?.name?.message}</AlertInput>
           </div>
           <div className="col-span-3">
@@ -222,21 +223,22 @@ function AssetInfo(props: { register: any, errors: any }) {
             <TypeSelect title={"Parent Asset"} placeholder={"Select parent asset"} data={['Parent 1', 'Parent 2']} />
           </div>
           <div className="col-span-3">
-            <InputField register={props.register} label="Project" name="name" />
-            <AlertInput>{props.errors?.name?.message}</AlertInput>
+            <TypeSelect title={"Project"} placeholder={"Select project"} data={['Project 1', 'Project 2']} />
           </div>
           <div className="col-span-3">
             <TypeSelect title={"Type"} placeholder={"Select asset type"} data={['Type 1', 'Type 2']} />
           </div>
-          <div className="col-span-9 grid grid-cols-8 gap-2">
-            <div className="col-span-4">
-              <InputField register={props.register} label="Residual Value" name="location" />
-              <AlertInput>{props.errors?.name?.message}</AlertInput>
-            </div>
-            <div className="col-span-4">
-              <InputField register={props.register} label="Residual Value" name="location" />
-              <AlertInput>{props.errors?.name?.message}</AlertInput>
-            </div>
+          <div className="col-span-3">
+            <InputField register={props.register} label="Serial Number" name="cost" />
+            <AlertInput>{props.errors?.name?.message}</AlertInput>
+          </div>
+          <div className="col-span-3">
+            <InputField register={props.register} label="Residual Value" name="location" />
+            <AlertInput>{props.errors?.name?.message}</AlertInput>
+          </div>
+          <div className="col-span-3">
+            <InputField register={props.register} label="Residual Value" name="location" />
+            <AlertInput>{props.errors?.name?.message}</AlertInput>
           </div>
 
           <div className="col-span-10">
@@ -245,17 +247,34 @@ function AssetInfo(props: { register: any, errors: any }) {
               label: "font-sans text-sm font-normal text-gray-600 text-light"
             }} />
           </div>
-          <div className="col-span-3">
-            <InputField register={props.register} label="Accounting Method" name="cost" />
-            <AlertInput>{props.errors?.name?.message}</AlertInput>
-          </div>
-          <div className="col-span-3">
-            <InputField register={props.register} label="Asset Lifetime" name="cost" />
-            <AlertInput>{props.errors?.name?.message}</AlertInput>
-          </div>
-          <div className="col-span-3">
-            <InputField register={props.register} label="Generated Asset Number" name="cost" />
-            <AlertInput>{props.errors?.name?.message}</AlertInput>
+
+          <div className='col-span-9 grid grid-cols-9 gap-2'>
+            <div className='col-span-6 grid grid-cols-6 gap-2'>
+              <div className="col-span-3">
+                <InputField register={props.register} label="Original Cost" name="cost" />
+                <AlertInput>{props.errors?.name?.message}</AlertInput>
+              </div>
+              <div className="col-span-3">
+                <InputField register={props.register} label="Current Cost" name="cost" />
+                <AlertInput>{props.errors?.name?.message}</AlertInput>
+              </div>
+              <div className="col-span-3">
+                <InputField register={props.register} label="Accounting Method" name="cost" />
+                <AlertInput>{props.errors?.name?.message}</AlertInput>
+              </div>
+              <div className="col-span-3">
+                <InputField register={props.register} label="Asset Lifetime" name="cost" />
+                <AlertInput>{props.errors?.name?.message}</AlertInput>
+              </div>
+            </div>
+            <div className="col-span-3 flex flex-col gap-2">
+              <div className='rounded-md flex items-center text-sm justify-center border-2 border-dashed w-full h-[65%] text-light-muted'>
+                <p>Preview of Barcode will appear here</p>
+              </div>
+              <button className="rounded-md border bg-tangerine-400 hover:bg-tangerine-500 outline-none focus:outline-none text-dark-primary h-[35%] font-semibold w-full px-4 py-2">
+                Generate Barcode
+              </button>
+            </div>
           </div>
 
         </div>
@@ -272,42 +291,35 @@ function AssetInfo(props: { register: any, errors: any }) {
       </Accordion.Control>
       <Accordion.Panel>
         <div className="grid grid-cols-9 gap-2">
-          {/* <div className="col-span-3 space-y-2">
-            <p className='text-sm text-gray-700'>Depreciation Start</p>
-            <DatePicker dropdownType="modal" size="sm" // value={props.value}
-              onChange={value => {// setValue("hired_date", value)
-              }} classNames={{
-                input: 'border-2 border-gray-400 h-11 rounded-md px-2'
-              }} // className="peer peer block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent px-3 text-sm text-gray-900 focus:border-tangerine-500 focus:outline-none focus:ring-0"
-            />
-          </div> */}
           <div className="col-span-3">
             <InputField register={props.register} label="Subsidiary" name="category" />
             <AlertInput>{props.errors?.name?.message}</AlertInput>
           </div>
           <div className="col-span-3">
-            <InputField register={props.register} label="Currency" name="category" />
-            <AlertInput>{props.errors?.name?.message}</AlertInput>
+            <TypeSelect title={"Currency"} placeholder={"Select currency type"} data={['Philippine Peso (Php)', 'US Dollar (USD)']} />
           </div>
           <div className="col-span-3">
             <InputField register={props.register} label="Custodian" name="category" />
             <AlertInput>{props.errors?.name?.message}</AlertInput>
           </div>
-          <div className="col-span-3">
-            <InputField register={props.register} label="Purchase Date" name="category" />
-            <AlertInput>{props.errors?.name?.message}</AlertInput>
+          <div className="col-span-3 space-y-2">
+            <p className='text-sm text-gray-700'>Purchase Date</p>
+            <DatePicker allowFreeInput size="sm" // value={props.value}
+              onChange={value => {// setValue("hired_date", value)
+              }} classNames={{
+                input: 'border-2 border-gray-400 h-11 rounded-md px-2'
+              }} // className="peer peer block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent px-3 text-sm text-gray-900 focus:border-tangerine-500 focus:outline-none focus:ring-0"
+            />
           </div>
           <div className="col-span-6">
             <InputField register={props.register} label="Physical Location" name="category" />
             <AlertInput>{props.errors?.name?.message}</AlertInput>
           </div>
           <div className="col-span-3">
-            <InputField register={props.register} label="Department" name="category" />
-            <AlertInput>{props.errors?.name?.message}</AlertInput>
+            <TypeSelect title={"Department"} placeholder={"Select department type"} data={['Department A', 'Department B']} />
           </div>
           <div className="col-span-3">
-            <InputField register={props.register} label="Class" name="category" />
-            <AlertInput>{props.errors?.name?.message}</AlertInput>
+            <TypeSelect title={"Class"} placeholder={"Select class type"} data={['Class A', ' B']} />
           </div>
           <div className="col-span-3">
             <InputField register={props.register} label="Location" name="category" />
@@ -329,12 +341,13 @@ function AssetInfo(props: { register: any, errors: any }) {
         <div className="grid grid-cols-9 gap-2">
           <div className="col-span-3 space-y-2">
             <p className='text-sm text-gray-700'>Date</p>
-            <DatePicker dropdownType="modal" size="sm" // value={props.value}
+            <DatePicker allowFreeInput size="sm" // value={props.value}
               onChange={value => {// setValue("hired_date", value)
               }} classNames={{
                 input: 'border-2 border-gray-400 h-11 rounded-md px-2'
               }} // className="peer peer block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent px-3 text-sm text-gray-900 focus:border-tangerine-500 focus:outline-none focus:ring-0"
             />
+            <i className=''></i>
           </div>
           <div className="col-span-3">
             <InputField register={props.register} label="Period" name="category" />

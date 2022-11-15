e9b5d0d29e7b8847049fd7397cf22891f1112ee7
Tenpai10 <pransarbey@gmail.com>
2022-11-15 15:45:58 +0800

    🧹 chore: advanced TS for nested object parsing

--- src/types/generic.ts
+++ src/types/generic.ts
@@ -1,4 +1,10 @@
 import { inferProcedureOutput } from "@trpc/server"
+import { z } from "zod"
+import { AssetCreateInput } from "../server/schemas/asset"
+import {
+  ManagementCreateInput,
+  ModelCreateInput,
+} from "../server/schemas/model"
 import { AppRouter } from "../server/trpc/router"
 
 //dynamic inference of type
@@ -7,3 +13,59 @@ export type VendorType = inferProcedureOutput<AppRouter["vendor"]["findOne"]>
 export type EmployeeType = inferProcedureOutput<
   AppRouter["employee"]["findOne"]
 >
+
+//asset field types
+export type InitialAssetFieldValues = z.infer<typeof AssetCreateInput>
+export type AssetFieldValues = z.infer<typeof AssetCreateInput>
+// export type ModelFieldValues = z.infer<typeof ModelCreateInput>
+// export type ManagementFieldValues = z.infer<typeof ManagementCreateInput>
+
+export type Join<K, P> = K extends string | number
+  ? P extends string | number
+    ? `${K}${"" extends P ? "" : "."}${P}`
+    : never
+  : never
+
+type Prev = [
+  never,
+  0,
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  10,
+  11,
+  12,
+  13,
+  14,
+  15,
+  16,
+  17,
+  18,
+  19,
+  20,
+  ...0[]
+]
+
+type Mdl<T, D extends number = 10> = [D] extends [never]
+  ? never
+  : T extends object
+  ? {
+      [K in keyof T]-?: K extends string | number
+        ? `${K}` | Join<K, Mdl<T[K], Prev[D]>>
+        : never
+    }[keyof T]
+  : ""
+
+type Mngmt<T, D extends number = 10> = [D] extends [never]
+  ? never
+  : T extends object
+  ? { [K in keyof T]-?: Join<K, Mngmt<T[K], Prev[D]>> }[keyof T]
+  : ""
+
+// export type AssetFieldValues = Mngmt<InitialAssetFieldValues>

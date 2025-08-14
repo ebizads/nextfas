adb40ef89e97650527abc83c557daec919c65c6f
Merkiel Llaneta <95353143+mbllaneta@users.noreply.github.com>
2025-08-14 17:06:29 +0800

    fix: bulk upload adjustment

--- src/components/atoms/accordions/AssetRecordsAccordion.tsx
+++ src/components/atoms/accordions/AssetRecordsAccordion.tsx
@@ -1,10 +1,10 @@
 import { Accordion } from "@mantine/core"
 // import { ExcelExportType } from "../../../types/asset"
-import { ExcelExportAssetType } from "../../../types/asset"
+import { ExcelAssetCheckerType } from "../../../types/asset"
 import { useState } from "react"
 
 const AssetRecordsAccordion = (props: {
-  incomingChanges: ExcelExportAssetType[]
+  incomingChanges: ExcelAssetCheckerType[]
 }) => {
   const [openedItem, setOpenedItem] = useState<string | null>(null)
 
@@ -22,14 +22,86 @@ const AssetRecordsAccordion = (props: {
           </Accordion.Control>
           <Accordion.Panel>
             <div className="flex flex-col gap-2">
-              <div className="flex gap-4">
-                <div className="flex flex-col gap-1 border-l-2 border-tangerine-500 px-4">
+              <div className="flex w-full gap-4">
+                <div className="flex w-full flex-col gap-1 border-l-2 border-tangerine-500 px-4">
                   <p className="text-sm text-light-secondary">
                     Incoming Change
                   </p>
-                  <pre className="text-sm">
+                  {/* <pre className="text-sm">
                     {JSON.stringify(asset, null, 2)}
-                  </pre>
+                  </pre> */}
+                  <div className="mt-4 flex flex-col gap-4 p-2 text-sm">
+                    <section className="grid w-full grid-cols-4 text-xs md:text-base">
+                      <div className="col-span-1">
+                        <p className=" font-light">Asset Name</p>
+                        <p className=" font-medium">
+                          {asset?.name && asset?.name.length > 0
+                            ? asset?.name
+                            : "--"}
+                        </p>
+                      </div>{" "}
+                      <div className="col-span-1">
+                        <p className=" font-light">Serial Number</p>
+                        <p className=" font-medium">
+                          {asset?.serial_no && asset?.serial_no.length > 0
+                            ? asset?.serial_no
+                            : "--"}
+                        </p>
+                      </div>{" "}
+                      <div className="col-span-1">
+                        <p className=" font-light">Brand</p>
+                        <p className=" font-medium">
+                          {asset?.brand && asset?.brand.length > 0
+                            ? asset?.brand
+                            : "--"}
+                        </p>
+                      </div>
+                    </section>
+                    <section className="grid w-full grid-cols-4 text-xs md:text-base">
+                      <div className="col-span-1">
+                        <p className=" font-light">Type</p>
+                        <p className=" font-medium">
+                          {asset?.type && asset?.type.length > 0
+                            ? asset?.type
+                            : "--"}
+                        </p>
+                      </div>{" "}
+                      <div className="col-span-1">
+                        <p className=" font-light">Caliber</p>
+                        <p className=" font-medium">
+                          {asset?.caliber && asset?.caliber.length > 0
+                            ? asset?.caliber
+                            : "--"}
+                        </p>
+                      </div>{" "}
+                      <div className="col-span-1">
+                        <p className=" font-light">Models</p>
+                        <p className=" font-medium">
+                          {asset?.models && asset?.models.length > 0
+                            ? asset?.models
+                            : "--"}
+                        </p>
+                      </div>
+                      <div className="col-span-1">
+                        <p className=" font-light">Action type</p>
+                        <p className=" font-medium">
+                          {asset?.action_type && asset?.action_type.length > 0
+                            ? asset?.action_type
+                            : "--"}
+                        </p>
+                      </div>
+                    </section>
+                    <section className="grid w-full  text-xs md:text-base">
+                      <div className="">
+                        <p className=" font-light">Description</p>
+                        <p className=" font-medium">
+                          {asset?.description && asset?.description.length > 0
+                            ? asset?.description
+                            : "--"}
+                        </p>
+                      </div>{" "}
+                    </section>
+                  </div>
                 </div>
               </div>
             </div>
